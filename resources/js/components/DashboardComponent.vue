cc52f37c3c4b43c94eec46f053cf61b017eb25f8
douglasrono <douglasronno@gmail.com>
2023-07-04 20:01:35 +0300

    fixed error in resource data

--- resources/js/components/DashboardComponent.vue
+++ resources/js/components/DashboardComponent.vue
@@ -10,7 +10,7 @@
           <h6 class="text-primary font-weight-normal lead text-dark">{{ offTrack }}</h6>
           <h4 class="mb-2 txt-dark">PARTNERS</h4>
           <span class="mdi mdi-arrow-up txt-danger icon-item"> 40%</span>
-          <small>vs last month</small>
+          <small> vs last month</small>
         </div>
 
         <div class="col-xl-3 col-sm-6 grid-margin">
@@ -18,7 +18,7 @@
           <h6 class="text-primary font-weight-normal lead text-dark">{{ atRisk }}</h6>
           <h4 class="mb- txt-dark">PARTNERS</h4>
           <span class="mdi mdi-arrow-down txt-warning icon-item"> -100%</span>
-          <small>vs last month</small>
+          <small> vs last month</small>
         </div>
 
         <div class="col-xl-3 col-sm-6 grid-margin">
@@ -27,7 +27,7 @@
           <h4 class="mb-2 txt-dark">PARTNERS</h4>
           <span class="mdi mdi-arrow-up txt-success icon-item"> +50%</span>
           <span class="ml-2"></span>
-          <small>vs last month</small>
+          <small> vs last month</small>
         </div>
       </div>
 
@@ -98,6 +98,75 @@
           </div>
         </div>
       </div>
+
+      <div class="my-5">
+        <div class="col-12 px-0">
+            <h3 class="text-dark">New partners</h3>
+          <div class="card">
+            <div class="card-header d-flex justify-content-between my-3">
+              <div>
+                <div class="input-container">
+                  <i class="mdi mdi-magnify mdi-icon"></i>
+                  <input class="input-field" type="text" placeholder="Search for partners">
+                </div>
+              </div>
+              <div>
+                <button class="btn btn-light p-3">
+                  <i class="mdi mdi-sort-variant text-dark"></i>
+                  Filters
+                </button>
+              </div>
+            </div>
+            <div class="card-body mb-5">
+              <div class="table-responsive">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>Partner</th>
+                      <th>Progress</th>
+                      <th>Members</th>
+                      <th>Departments</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="partner in partners" :key="partner.id">
+                      <td>
+                        <div class="d-flex align-items-center">
+                          <img :src="partner.image" alt="image" />
+                          <span class="pl-2">{{ partner.name }}</span>
+                        </div>
+                        <div class="d-flex align-items-center">
+                          <span class="active-period txt-gray">Active Period: {{ '8 months ago' }}</span>
+                        </div>
+                      </td>
+                      <td class="stats">
+                        <span>{{ '48%' }}</span>
+                        <div class="progress">
+                          <div class="progress-bar bg-primary" role="progressbar" :style="{ width: '48%' }" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                      </td>
+                      <td class="td-members">
+                        <img v-for="member in partner.members" :key="member.id" src="member.image" alt="image" />
+                      </td>
+                      <td>
+                        <span class="department-tag" v-for="department in partner.departments" :key="department.id">{{ department.name }}</span>
+                      </td>
+                      <td>
+                        <button class="btn btn-pri  d-flex align-items-center">
+                          <i class="mdi mdi-eye-outline text-light mx-2"></i>
+                          <a :href="'/partners/' + partner.id" class="text-light">View Details</a>
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <pagination :total="totalPages" :current="currentPage" @page-change="handlePageChange"></pagination>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </template>
 
@@ -134,14 +203,16 @@
       fetchPartners(){
                 let uri =this.base_url+`api/v1/partner-list`;
                 axios.get(uri).then((response) => {
-                    this.partners = response.data.data;
+                    this.partners = response.data;
                 });
             },
     },
   };
   </script>
 
-  <style>
-  /* Add your custom styles here */
+  <style scoped>
+
+
+
   </style>
 
