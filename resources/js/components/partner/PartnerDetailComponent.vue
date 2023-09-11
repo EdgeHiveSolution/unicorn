373cee2a28ca070874cfa0e2721f31f96fa45a22
Edu-Sam <edungugi20@gmail.com>
2023-09-11 09:07:26 +0300

    changed user role to 4

--- resources/js/components/partner/PartnerDetailComponent.vue
+++ resources/js/components/partner/PartnerDetailComponent.vue
@@ -1206,7 +1206,7 @@
                         </div>
                     </div>
 
-                    <div v-else-if="loggedUser.user_role_id === 14">
+                    <div v-else-if="loggedUser.user_role_id === 4">
                         <div
                             class="col-12 px-0"
                             v-for="kpi in kpis"
