534ead921b4d720db0928552eaccef6ed7e9231f
TechTribeInnovators <github@techtribeinnovations.com>
2024-01-24 19:33:03 +0300

    asset

--- config/app.php
+++ config/app.php
@@ -56,9 +56,11 @@ return [
     |
     */
 
-    #'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost'),
    # 'url' => env('APP_URL', 'https://unicon1-4e6bc01d9a83.herokuapp.com'),
-   'url' => env('APP_URL', 'https://tactiv.clifford.co.ke'),
+   #'url' => env('APP_URL', 'https://tactiv.clifford.co.ke'),
+   //'url' => env('APP_URL', 'https://tactiv.techtribeinnovations.com'),
+
    
 
 
