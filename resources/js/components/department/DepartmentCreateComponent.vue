ec7523bfd2c25dbf5e1122638b5bdc1f559d6a0b
TechTribeInnovators <github@techtribeinnovations.com>
2024-05-30 18:34:02 +0300

    validations

--- resources/js/components/department/DepartmentCreateComponent.vue
+++ resources/js/components/department/DepartmentCreateComponent.vue
@@ -307,8 +307,6 @@
 </template>
 
 <script>
-/*import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";*/
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
